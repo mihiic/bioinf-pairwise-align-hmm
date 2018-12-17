7e6db84989a9360aeb246f80c0d0b60d0c038f9f
mjularic <matej.jularic2@gmail.com>
2018-12-17 17:34:13 +0100

    Finished script for calculating statistics

--- scripts/get_statistic.py
+++ scripts/get_statistic.py
@@ -19,20 +19,22 @@ def get_state(x, y):
 
     #state MATCH
     if (x == 'A' or x == "G" or x == "C" or x == "T") and (y == 'A' or y == "G" or y == "C" or y == "T"):
-        return 0
+        return 1
 
     #state Ix
     if (x == 'A' or x == "G" or x == "C" or x == "T") and y == '-':
-        return 1
+        return 2
 
     #state Iy
     if x == '-' and (y == 'A' or y == "G" or y == "C" or y == "T"):
-        return 2
+        return 3
 
 
-transitions_matrix = [[0, 0, 0],
-                      [0, 0, 0],
-                      [0, 0, 0]]
+transitions_matrix = [[0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0]]
 
 emissions_matrix = [[0, 0, 0, 0, 0],
                     [0, 0, 0, 0, 0],
@@ -40,10 +42,13 @@ emissions_matrix = [[0, 0, 0, 0, 0],
                     [0, 0, 0, 0, 0],
                     [0, 0, 0, 0, 0]]
 
+
 #states
-match_state = 0
-ix_state = 1
-iy_state = 2
+begin_state = 0
+match_state = 1
+ix_state = 2
+iy_state = 3
+end_state = 4
 
 #symbols
 gap = 0
@@ -54,7 +59,6 @@ G = 4
 
 
 directory = "pairwise_alignment_HIV"
-
 
 all_files = os.listdir(directory)
 
@@ -81,9 +85,36 @@ for filename in all_files:
         current_state = get_state(nucleobase1, nucleobase2)
 
         if last_state is None:
+            transitions_matrix[begin_state][current_state] += 1
             last_state = current_state
 
         else:
             transitions_matrix[last_state][current_state] += 1
             last_state = current_state
+    transitions_matrix[last_state][end_state] += 1
 
+
+sum_begin = 0
+sum_match = 0
+sum_ix = 0
+sum_iy = 0
+
+for i in range(5):
+    sum_begin += transitions_matrix[begin_state][i]
+    sum_match += transitions_matrix[match_state][i]
+    sum_ix += transitions_matrix[ix_state][i]
+    sum_iy += transitions_matrix[iy_state][i]
+
+for i in range(5):
+    transitions_matrix[begin_state][i] /= sum_begin
+    transitions_matrix[match_state][i] /= sum_match
+    transitions_matrix[ix_state][i] /= sum_ix
+    transitions_matrix[iy_state][i] /= sum_iy
+
+for i in range(5):
+    row = ""
+    for j in range(5):
+        row = row + str(transitions_matrix[i][j]) + " "
+
+    row = row + "\n"
+    print(row)
