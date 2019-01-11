b409a1def99c32d12773e1ee241edd03e91d642a
Mihael <mihic.medan@gmail.com>
2019-01-11 09:00:45 +0100

    done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 #include "alignment_model.h"
 
 int main(int argc, char** argv) {
-    AlignmentModel model(4096);
-    model.run(argv[1], argv[2], argv[3], argv[4], argv[5]); //
+    if (argc == 7) {
+        AlignmentModel model((unsigned long)stoi(argv[6]));
+        model.run(argv[1], argv[2], argv[3], argv[4], argv[5]); //
+    } else {
+        AlignmentModel model(4096);
+        model.run(argv[1], argv[2], argv[3], argv[4], argv[5]); //
+    }
     return 0;
 }
